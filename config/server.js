b26880b53339fe6529014ba01f95bc9ce5f8a822
Paulo <pauloh16.pdr@gmail.com>
2021-07-20 12:03:19 -0300

    Iniciar projeto e configurar

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '329a22762fa565fd72ef083507f413dd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '289138c4-507c-41bb-9d5d-8bf1b9722f95'
+};
