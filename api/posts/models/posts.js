53c9ad97de385c9cd9bdbdaca5454b851fe23640
Paulo <pauloh16.pdr@gmail.com>
2021-08-21 16:51:19 -0300

    Adicionar life hooks para netlift

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -1,8 +1,17 @@
 'use strict';
 
+const axios = require('axios');
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  async afterCreate(result, data) {
+    axios.post('https://api.netlify.com/build_hooks/612158c2f96e118446a89233');
+  },
+  async afterUpdate(result, params, data) {
+    axios.post('https://api.netlify.com/build_hooks/612158c2f96e118446a89233');
+  }
+};
